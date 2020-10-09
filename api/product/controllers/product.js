f28f8ecb4da2b1f6c5635cf29b3d1495c1df0ca4
Patrick Charriere <patrick.charriere.pro@gmail.com>
2020-10-09 14:23:44 +1000

    Hide confidential provider from product

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,28 @@
-'use strict';
+'use strict'
 
-/**
- * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require('strapi-utils')
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities
+    if (ctx.query._q) {
+      entities = await strapi.services.product.search(ctx.query)
+    } else {
+      entities = await strapi.services.product.find(ctx.query)
+    }
+
+    return entities.map(entity => {
+      const product = sanitizeEntity(entity, {
+        model: strapi.models.product,
+      });
+      
+      if (product.source_product) {
+        delete product.source_product
+      }
+
+      return product
+
+    })
+
+  },
+}
